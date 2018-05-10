fa9e58d3d226015caf5aabb25e51232c2834adca
zheming <38298048+zhemingg@users.noreply.github.com>
2018-05-10 16:41:02 -0700

    add my basic information like bio, education.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,74 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio = {
+ 	"name" : "Zheming Gao",
+ 	"role" : "Graduate Student",
+ 	"contacts" : {
+ 		"mobile" : "(213)-7061918",
+ 		"email" : "gao.zh@husky.neu.edu",
+ 		"github" : "https://github.com/zhemingg",
+ 		"location" : "San Jose"
+ 	},
+ 	"welcomeMessage" : "Welcome to my home page",
+	"skills" : ["Java", "C", "C++", "JavaScripts", "HTML", "CSS"],
+	"biopic" : "images/me.jpg",
+	"display" : "to be filled later"
+ }
+
+ var education = {
+ 	"Schools" : [
+ 	{
+ 		"name" : "University of Science & Technology",
+ 		"location" : "Changsha, Hunan, China",
+ 		"degree" : "Bachelor",
+ 		"majors" : "Civil Engineering",
+ 		"date" : "June, 2014",
+ 		"url" : "http://www.csust.edu.cn/"
+ 	},
+ 	{
+ 		"name" : "University of Southern California",
+ 		"location" : "Los Angeles, California, the United State",
+ 		"degree" : "Master of Science",
+ 		"majors" : "Civil Engineering (Structure Engineering)",
+ 		"date" : "May, 2017",
+ 		"url" : "https://www.usc.edu/"
+ 	},
+ 	{
+ 		"name" : "Northeastern University - SV compus",
+ 		"location" : "San Jose, California, the United State",
+ 		"degree" : "Master of Science",
+ 		"majors" : "Computer Science",
+ 		"date" : "May, 2020",
+ 		"url" : "http://www.northeastern.edu/siliconvalley/"
+ 	}
+ 	],
+ 	"display" : "TO BE FILLED LATER"
+ }
+
+ var work = {
+ 	"status" : "Looking for intern of Spring 2019",
+ 	"display": "function taking no parameters"
+ }
+
+ var projects = {
+ 	projects : [
+ 	{
+ 	"title": "home page", 
+    "dates": "May, 2018 - June, 2018",
+    "description": "A private home page builded with HTML, CSS and JavaScripts",
+    "images": "array with string urls"
+ 	},
+ 	{
+ 	"title": "Web Development Course project", 
+    "dates": "May, 2018 - August, 2018",
+    "description": "A Course project that create a websites that are dynamic, data driven, and interactive with other 2 parterners.",
+    "images": "array with string urls",
+    "display": "function taking no parameters"
+ 	}
+ 	]
+ }
+
+
+
